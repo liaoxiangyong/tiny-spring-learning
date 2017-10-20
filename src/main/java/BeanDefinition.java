e0659c34cb8ffa93814981837732e83f489b58fa
BrookLiao <qjliaoxy@163.com>
2017-10-20 10:30:52 +0800

    first-step-bean-and-beanFactory

--- src/main/java/BeanDefinition.java
+++ src/main/java/BeanDefinition.java
@@ -1,6 +1,19 @@
 /**
  * Created by LXY on 2017/9/15.
  */
-public class BeanDefinition {
+public class BeanDefinition {       //bean定义类
 
+    private Object bean;        //bean
+
+    private Class beanClass;        //bean所属的类
+
+    private String beanClassName;   //bean的类名
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
 }
---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by LXY on 2017/10/20.
+ */
+public class HelloWorld {
+    public void helloWorld(){
+        System.out.println("Hello World!");
+    }
+}
